36d01c1340116d63974c2b3ee53d0bd0a3cee1a8
Prasad <prasad7779@gmail.com>
2015-10-21 11:52:22 +0200

    new color

--- PulseMobileClients/PulseAndroidClient/gen/ch/ethz/coss/nervous/pulse/R.java
+++ PulseMobileClients/PulseAndroidClient/gen/ch/ethz/coss/nervous/pulse/R.java
@@ -19,6 +19,8 @@ public final class R {
     public static final class color {
         public static final int black=0x7f050001;
         public static final int gray=0x7f050002;
+        public static final int nnorange=0x7f050003;
+        public static final int nnorangel=0x7f050004;
         public static final int white=0x7f050000;
     }
     public static final class dimen {
