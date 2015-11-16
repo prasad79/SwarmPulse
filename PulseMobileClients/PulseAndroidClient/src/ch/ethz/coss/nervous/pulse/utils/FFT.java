f7cfd5f3b151315f9d2d86d3ab0e95b43f7fbd5d
Prasad <prasad7779@gmail.com>
2015-11-16 13:10:10 +0100

    changed Copyright information

--- PulseMobileClients/PulseAndroidClient/src/ch/ethz/coss/nervous/pulse/utils/FFT.java
+++ PulseMobileClients/PulseAndroidClient/src/ch/ethz/coss/nervous/pulse/utils/FFT.java
@@ -4,7 +4,7 @@ package ch.ethz.coss.nervous.pulse.utils;
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 ETH Zurich.
+ * Copyright (c) 2015 ETH Zürich.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
